1ecde2ec7c5a9b1da5e50205e029a39a7b2dcaee
Arm <armm4sonic@gmail.com>
2023-07-14 14:44:25 +0700

    2023-07-14 - Lab 2

---
+++ LAB_2_-_Ultrasonic.ino
@@ -0,0 +1,50 @@
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int pingPin = 13;
+int inPin = 12;
+
+
+void setup() {
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0); // กำหนดให้ เคอร์เซอร์ อยู่ตัวอักษรตำแหน่งที่0 แถวที่ 1 เตรียมพิมพ์ข้อความ
+  lcd.print("LCD1602 I2c Test"); //พิมพ์ข้อความ "LCD1602 I2c Test"
+  lcd.setCursor(2, 1); // กำหนดให้ เคอร์เซอร์ อยู่ตัวอักษรกำแหน่งที3 แถวที่ 2 เตรียมพิมพ์ข้อความ
+  lcd.print("myarduino.net"); //พิมพ์ข้อความ "myarduino.net"  
+  
+Serial.begin(9600);
+}
+
+void loop()
+{
+long duration, cm;
+
+pinMode(pingPin, OUTPUT);
+
+
+digitalWrite(pingPin, LOW);
+delayMicroseconds(2);
+digitalWrite(pingPin, HIGH);
+delayMicroseconds(5);
+digitalWrite(pingPin, LOW);
+pinMode(inPin, INPUT);
+duration = pulseIn(inPin, HIGH);
+
+cm = microsecondsToCentimeters(duration);
+
+Serial.print(cm);
+Serial.print("cm");
+Serial.println();
+delay(100);
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+// The ping travels out and back, so to find the distance of the
+// object we take half of the distance travelled.
+return microseconds / 29 / 2;
+}
